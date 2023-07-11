e49976f0631936f4f73a8c0229fb0ecc88e50ce6
Eldinosaur <eldinosaur7u7@outlook.com>
2023-07-11 13:02:48 -0500

    servicio de cancelar solicitud

---
+++ servicios/cancelarSolicitud.php
@@ -0,0 +1,23 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
+include 'conexionBD.php';
+$id = $_GET['id'];
+
+
+$result = false;
+
+    $sqlInsertInvernadero = "UPDATE `solicitudes` SET `Estado_Solicitud`='4' WHERE ID_Solicitud =".$id;
+    if ($mysqli->query($sqlInsertInvernadero) === TRUE) {
+        $result = true;
+    } else {
+        echo json_encode("error " . $sqlinsertar . $mysqli->error);
+    }
+
+
+$mysqli->close();
+echo $result;
+?>
